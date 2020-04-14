3b76a364022ed488fb092ec8319801780135fbe1
Nishchal Jain <nishchaljain786@gmail.com>
2020-04-14 09:34:20 +0530

    Refactored the code by creating a separate module for employee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CreateEmployeeComponent } from './employee/create-employee.component';
-import { ListEmployeeComponent } from './employee/list-employee.component';
-import { EmployeeService } from './employee/employee.service'
+
 import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { EmployeeModule } from './employee/employee.module';
+import { EmployeeService } from './employee/employee.service'
+
+import { AppComponent } from './app.component';
+
+import { HomeComponent } from './home.component';
+import { PagenotfoundComponent } from './pagenotfound.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateEmployeeComponent,
-    ListEmployeeComponent
+    HomeComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    EmployeeModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
